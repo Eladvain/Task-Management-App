67b1b8e7362d3c166ac075ddd5ff2014bd411095
Eladvain <eladvainberg@gmail.com>
2023-06-11 18:38:09 +0300

    update

--- frontend/myapp/src/components/EditTask.js
+++ frontend/myapp/src/components/EditTask.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const EditTask = () => {
+
+  const [statusChecked, setStatusChecked] = useState(false);
+
+  const handleChange = async () => {
+    console.log(statusChecked);
+    setStatusChecked(!statusChecked);
+    console.log(statusChecked);
+
+    if(statusChecked === true){
+      console.log("in update task");
+    }
+  };
+
+  // async function updateTask() {
+    
+  //   console.log("checkBox = "+checkBox)
+  //   // console.log("in update task");
+  //   if (checkBox.checked){
+  //     console.log("in update task");
+  //     const sel = document.createElement("select");
+  //     const opt1 = document.createElement("option");
+  //     const opt2 = document.createElement("option");
+
+  //     opt1.value = "in process";
+  //     opt1.text = "Option: in process";
+
+  //     opt2.value = "done";
+  //     opt2.text = "Option: done";
+
+  //     sel.add(opt1, null);
+  //     sel.add(opt2, null);
+
+  //     document.getElementById("appendToUpdate").appendChild(sel);
+  //   } 
+  // }
+
   return (
     <div>
-      <h1>Helloooooo</h1>
+      <h1 className='choose-update'>choose which field to update</h1>
+      <label htmlFor='status'>Status:</label> 
+      <input type="checkbox"
+             id="status" 
+             checked={statusChecked}
+             onChange={handleChange}>
+       </input>
+      <div className='appendToUpdate'></div>
     </div>
   )
 }
