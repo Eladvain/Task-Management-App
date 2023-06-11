4c57c4d0b48e6d0ee1b38377d63ede83d3df4e6a
Eladvain <eladvainberg@gmail.com>
2023-06-11 17:13:57 +0300

    start edit task page

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import TaskList from './components/taskList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EditTask from './components/EditTask';
 
 function App() {
   return (
     <div className="App">
-      <TaskList/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<TaskList/>} ></Route>
+          <Route path="editTask" element = {<EditTask/>} ></Route>
+        </Routes>
+      </BrowserRouter>  
+      
     </div>
   );
 }
--- frontend/myapp/src/components/TaskItem.js
+++ frontend/myapp/src/components/TaskItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import taskItemCss from '../CSS/taskItem.css';
+import { Link, Outlet } from 'react-router-dom'
 
 const TaskItem = ({taskItem}) => {
   return (
     <div className='task-container'>
+      <Link to = "/editTask" state={{task : taskItem}}>edit task</Link>
+      <Outlet/>
       <h1 className='nameOfTask'>{taskItem.name}</h1>
       <label> desc :{taskItem.description !== "" ? taskItem.description : "none"}  </label>
       <br/>
