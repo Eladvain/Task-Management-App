c9ad3873dea494029e3b83ca3678b625cba21f96
Eladvain <eladvainberg@gmail.com>
2023-06-27 14:50:15 +0300

    start login page

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import TaskList from './components/taskList';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EditTask from './components/EditTask';
+import LogIn from './components/LogIn';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element = {<TaskList/>} ></Route>
+          <Route path="/home" element = {<TaskList/>} ></Route>
+          <Route path="/" element = {<LogIn/>} ></Route>
           <Route path="editTask" element = {<EditTask/>} ></Route>
         </Routes>
       </BrowserRouter>  
---
+++ frontend/myapp/src/components/LogIn.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logIn from '../CSS/logIn.css'
+
+const LogIn = () => {
+  return (
+    <div className='log-in-container'>
+      <div className='wrap-log-in'>
+        <div className='log-in-pic'>
+          <img className='img-login' src="https://colorlib.com/etc/lf/Login_v1/images/img-01.png" alt="png" />
+        </div>
+        <div>
+          <h1>hello world</h1>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default LogIn
