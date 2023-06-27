017826485b3cc09e2007f213c784a169feb760a4
Eladvain <eladvainberg@gmail.com>
2023-06-27 16:19:40 +0300

    login page

--- frontend/myapp/src/components/LogIn.js
+++ frontend/myapp/src/components/LogIn.js
@@ -8,9 +8,28 @@ const LogIn = () => {
         <div className='log-in-pic'>
           <img className='img-login' src="https://colorlib.com/etc/lf/Login_v1/images/img-01.png" alt="png" />
         </div>
-        <div>
-          <h1>hello world</h1>
-        </div>
+        <form className='form-log-in'>
+          <div className='div-form'>
+          <span className='span-loging'>Member Login</span>
+          <div className='name-input-div'>
+            {/* <span className='name-span'>Name : </span> */}
+            <input className='name-input'
+                   type="text"
+                   placeholder='Name'/>
+
+          </div>
+          <div className='password-input-div'>
+            {/* <span className='name-span'>Password : </span> */}
+            <input className='password-input'
+                   type="text"
+                   placeholder='Password'/>
+
+          </div>
+          </div>
+        </form>
+        
+          
+        
       </div>
       
     </div>
