dbca087fc857f62323ac0dd744eb6940fbea4363
Eladvain <eladvainberg@gmail.com>
2023-06-27 18:41:52 +0300

    new signin page

--- frontend/myapp/src/components/NewAcount.js
+++ frontend/myapp/src/components/NewAcount.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import newAcount from '../CSS/newAcount.css'
+// import login from '../CSS/logIn.css'
 
 const NewAcount = () => {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className='div-container'>
+      <div className='newAcount-div'>
+        <form className='newAcount-form'>
+            <span className='newAcount-span'>New Member SignIn</span>
+            <div className='new-name-div'>
+            <input className='new-name-input'
+                   type="text"
+                   placeholder='Name'/>
+
+          </div>
+          <div className='new-password-div'>
+            <input className='new-name-input'
+                   type="text"
+                   placeholder='Password'/>
+
+          </div>
+          <div className='button-signin-div'>
+            <button className='button-signin'>SignIn</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
