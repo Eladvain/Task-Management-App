28e97f08c184f5c52aae5256a0579ef07f0749b0
Eladvain <eladvainberg@gmail.com>
2023-06-29 13:42:17 +0300

    update login

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -51,8 +51,65 @@ async function get_all_users_name(req, res)
 
 }
 
+async function get_all_users(req,res)
+{
+
+} 
+
+async function log_in(req, res)
+{
+  const {name, password} = req.body;
+  const sqlQuery = `SELECT * FROM user WHERE name='${name}'`;
+  
+  connection.query(sqlQuery, async function(err, result) {
+    if(err){
+      throw err.message;
+    }
+    
+    console.log("users = "+JSON.stringify(result));
+    try{
+          let resp = await bcrpyt.compare(password, result[0].password);
+          if(resp === true)
+          {
+            console.log("the same password");
+          }
+          else{
+            console.log("password not correct");
+          }
+        }catch(e){
+          console.log(e.message);
+        }
+  res.send({users : result});
+  })
+  
+
+//   try{
+//     let resp = await bcrpyt.compare(req.body.password, user.password);
+//      if(resp == true)
+//      { 
+//      const user_email = user.email;
+//      const token = jwt.sign({ user_email }, "secret", { expiresIn: "1d" });
+     
+//      const token_string = "access_token=" + token;
+//      res.setHeader('Set-Cookie', token_string +"; path=/") // create a cookie in the browser
+//      res.send({token:token,
+//                msg:"You sign in"});	 
+//       } 
+//      else{
+//        res.status(StatusCodes.FORBIDDEN);
+//        res.send({ msg: "Error password is not valid" });
+//        return;
+//      }
+     
+//  } catch (err) {
+//    console.log("Error in Auth", err);
+//    res.status( StatusCodes.BAD_REQUEST );
+//  }
+}
+
 
 router.post('/signin',  (req, res) => { create_new_user(req, res) });
+router.post('/login',  (req, res) => { log_in(req, res) });
 
 router.get('/usersName',  (req, res) => { get_all_users_name(req, res) });
 
